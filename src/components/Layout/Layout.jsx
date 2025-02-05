f0cddc38bf11471adffdec4dcde1acf5f8e9c831
mkothm <mkothman.jr@gmail.com>
2025-02-05 17:09:33 +0100

    build error resolved

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Footer from "./Footer";
 import ThemeToggle from "./ThemeToggle";
-import Navbar from "./NavBar";
+import Navbar from './NavBar'
 
 const Layout = ({ children }) => {
   return (
